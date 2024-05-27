859acaf139c7b6d602e66d306069fa8abead5bba
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-05-27 18:54:44 +0000

    lazy loading all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,103 @@
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import {
-  MoviesPage,
-  TvShowsPage,
-  TvShowDetails,
-  PersonPage,
-  HomePage,
-  MovieDetails,
-  PersonDetails,
-  NotFound,
-} from "./pages";
+import React, { Suspense } from "react";
+
+const Navbar = React.lazy(() => import("./components/Navbar"));
+const Footer = React.lazy(() => import("./components/Footer"));
+const HomePage = React.lazy(() => import("./pages/HomePage"));
+const MovieDetails = React.lazy(() => import("./pages/MovieDetails"));
+const MoviesPage = React.lazy(() => import("./pages/MoviesPage"));
+const TvShowDetails = React.lazy(() => import("./pages/TvShowDetails"));
+const TvShowsPage = React.lazy(() => import("./pages/TvShowsPage"));
+const PersonDetails = React.lazy(() => import("./pages/PersonDetails"));
+const PersonPage = React.lazy(() => import("./pages/PersonPage"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
       <>
-        <Navbar />
+        <Suspense fallback={<div>Navbar...</div>}>
+          <Navbar />
+        </Suspense>
         <div className="w-full h-20"></div>
-        <Outlet />
-        <Footer />
+        <Suspense fallback={<div>Content...</div>}>
+          <Outlet />
+        </Suspense>
+        <Suspense fallback={<div>Footer...</div>}>
+          <Footer />
+        </Suspense>
       </>
     ),
     children: [
       {
         path: "/",
-        element: <HomePage />,
+        element: (
+          <Suspense fallback={<div>HomePage...</div>}>
+            <HomePage />
+          </Suspense>
+        ),
       },
       {
         path: "/movie/details/:id",
-        element: <MovieDetails />,
+        element: (
+          <Suspense fallback={<div>MovieDetails...</div>}>
+            <MovieDetails />
+          </Suspense>
+        ),
       },
       {
         path: "/movies/:category",
-        element: <MoviesPage />,
+        element: (
+          <Suspense fallback={<div>MoviesPage...</div>}>
+            <MoviesPage />
+          </Suspense>
+        ),
       },
       {
         path: "/tv/details/:id",
-        element: <TvShowDetails />,
+        element: (
+          <Suspense fallback={<div>TvShowDetails...</div>}>
+            <TvShowDetails />
+          </Suspense>
+        ),
       },
       {
         path: "/tv/:category",
-        element: <TvShowsPage />,
+        element: (
+          <Suspense fallback={<div>TvShowsPage...</div>}>
+            <TvShowsPage />
+          </Suspense>
+        ),
       },
       {
         path: "/people/details/:id",
-        element: <PersonDetails />,
+        element: (
+          <Suspense fallback={<div>PersonDetails...</div>}>
+            <PersonDetails />
+          </Suspense>
+        ),
       },
       {
         path: "/people/:category",
-        element: <PersonPage />,
+        element: (
+          <Suspense fallback={<div>PersonPage...</div>}>
+            <PersonPage />
+          </Suspense>
+        ),
       },
       {
         path: "*",
-        element: <NotFound />,
+        element: (
+          <Suspense fallback={<div>NotFound...</div>}>
+            <NotFound />
+          </Suspense>
+        ),
       },
     ],
   },
 ]);
-function App() {
+interface IAppProps {}
+const App: React.FC<IAppProps> = () => {
   return (
     <div className="relative w-screen min-h-screen flex justify-center bg-white dark:bg-gray-800 text-black dark:text-white">
       <div className="w-full mx-auto max-container">
@@ -66,6 +105,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/DiscoverPage.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+interface IDiscoverPageProps {}
+
+export const DiscoverPage: React.FunctionComponent<IDiscoverPageProps> = () => {
+  return <></>;
+};
