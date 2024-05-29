eb0e4bbe7eccb7c6a1a93b9e696ddd445169af32
sandesh ghoti <sandesh.ghoti@gmail.com>
2024-05-29 06:27:53 +0000

    memoization to rating component

--- src/components/Ratings.tsx
+++ src/components/Ratings.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import { Star } from "lucide-react";
 
 interface RatingsProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -10,53 +10,55 @@ interface RatingsProps extends React.HTMLAttributes<HTMLDivElement> {
   variant?: "default" | "destructive" | "yellow";
 }
 
-const Ratings: React.FunctionComponent<RatingsProps> = ({
-  rating,
-  totalStars = 5,
-  size = 20,
-  fill = true,
-  Icon = Star,
-  variant = "default",
-  ...props
-}) => {
-  const fullStars = Math.floor(rating);
-  const partialStar =
-    rating % 1 > 0 ? (
-      <PartialStar
-        fillPercentage={rating % 1}
-        size={size}
-        className={getStarClassName(variant, true)}
-        Icon={Icon}
-      />
-    ) : null;
-
-  return (
-    <div className="flex items-center gap-1" {...props}>
-      {[...Array(fullStars)].map((_, i) => (
-        <Icon
-          key={i}
+const Ratings: React.FunctionComponent<RatingsProps> = memo(
+  ({
+    rating,
+    totalStars = 5,
+    size = 20,
+    fill = true,
+    Icon = Star,
+    variant = "default",
+    ...props
+  }) => {
+    const fullStars = Math.floor(rating);
+    const partialStar =
+      rating % 1 > 0 ? (
+        <PartialStar
+          fillPercentage={rating % 1}
           size={size}
           className={getStarClassName(variant, true)}
-          fill={fill ? "currentColor" : "transparent"}
+          Icon={Icon}
         />
-      ))}
-      {partialStar}
-      {[...Array(totalStars - fullStars - (partialStar ? 1 : 0))].map(
-        (_, i) => (
+      ) : null;
+
+    return (
+      <div className="flex items-center gap-1" {...props}>
+        {[...Array(fullStars)].map((_, i) => (
           <Icon
-            key={i + fullStars + 1}
+            key={i}
             size={size}
-            className={getStarClassName(variant, false)}
-            fill={"transparent"}
+            className={getStarClassName(variant, true)}
+            fill={fill ? "currentColor" : "transparent"}
           />
-        )
-      )}
-      <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
-        {rating}
-      </span>
-    </div>
-  );
-};
+        ))}
+        {partialStar}
+        {[...Array(totalStars - fullStars - (partialStar ? 1 : 0))].map(
+          (_, i) => (
+            <Icon
+              key={i + fullStars + 1}
+              size={size}
+              className={getStarClassName(variant, false)}
+              fill={"transparent"}
+            />
+          )
+        )}
+        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+          {rating}
+        </span>
+      </div>
+    );
+  }
+);
 
 interface PartialStarProps {
   fillPercentage: number;
